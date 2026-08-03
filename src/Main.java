a6a401ed2d924e6c1549e2ca9052fb238fc5dd6c
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-03 20:44:39 -0300

    Menu incial

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        }
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Para iniciarmos seu atendimento por favor informe os seguintes dados:");
+        System.out.println();
+        System.out.println("Qual o seu nome:");
+        String nome = leitura.nextLine();
+        System.out.println("Informe qual o saldo que tem no banco: ");
+        double saldoAtual = leitura.nextDouble();
+        System.out.println("=====================================");
+        System.out.println("DADOS DA CONTA:");
+        System.out.println("Nome: " + nome);
+        System.out.println("Saldo atual: R$" + saldoAtual);
+        String menu = """
+                =====================================,
+                1-Consultar saldo,
+                2-Realizar deposito,
+                3-Realizar transferencia,
+                4-Sair
+                """;
 
     }
 }
