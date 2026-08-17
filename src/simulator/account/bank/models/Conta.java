91bda9a6a0e7850221ba3ff5b4253ec0202e802d
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-17 15:26:39 -0300

    Criando atributos para a classe

--- src/simulator/account/bank/models/Conta.java
+++ src/simulator/account/bank/models/Conta.java
@@ -1,4 +1,12 @@
 package simulator.account.bank.models;
 
 public class Conta {
+    private String titularConta;
+    private String cpf;
+    private double saldoConta;
+    private boolean contaCorrente;
+    private boolean contaPoupanca;
+
+
+
 }
