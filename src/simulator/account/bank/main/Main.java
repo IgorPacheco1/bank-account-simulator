c7d8bb2c3379e0256650a3493e8a724eeab436c4
IgorPacheco1 <igopacheco7@gmail.com>
2026-08-23 18:14:35 -0300

    Observações para melhorias

--- src/simulator/account/bank/main/Main.java
+++ src/simulator/account/bank/main/Main.java
@@ -4,6 +4,9 @@ import simulator.account.bank.display.Menu;
 
 public class Main {
     public static void main(String[] args) {
+        // Lembrar das observações do professor, como eu imaginei essa classe main com esse tanto de metodo não se enquadra na forma certa de POO
+        // Posteriormente mudar menu para uma classe static para não precisar instanciar e distribuir os metodos para outras classes
+
         Menu menu = new Menu();
         menu.exibirMenu();
     }
